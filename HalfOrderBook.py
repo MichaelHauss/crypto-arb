52e70945a0990c8d3b0aedad6203909b340c1cca
connerjager <connerjager@gmail.com>
2017-09-21 23:49:29 -0400

    merging into development

--- HalfOrderBook.py
+++ HalfOrderBook.py
@@ -1,11 +1,12 @@
-import heapq
 from sortedcontainers import SortedDict
 
 class HalfOrderBook(object):
-	def __init__ (self, asc=True):
-		self.ids = {}
-		self.sizes = SortedDict()
-		self.asc = asc
+	def __init__ (self, asc=True, orders=[]):
+		self.ids    = {}
+		self.orders = SortedDict()
+		self.asc    = asc
+
+		self.updateMany (orders)
 
 	def update (self, order):
 		assert "id" in order and "price" in order and "size" in order
@@ -16,32 +17,34 @@ class HalfOrderBook(object):
 		# does not change its id but changing the price would (i.e. 
 		# price of a specific order id doesn't change)
 		assert newId not in self.ids \
-			   or self.ids[newId]["price"] == order["price"]
+			or self.ids[newId]["price"] == order["price"]
 
+		# look up old order to calculate delta
 		oldSize = self.ids[newId]["size"] if newId in self.ids else 0
 		self.ids[newId] = order
 		
+		# update order book
 		sizeDelta = order["size"] - oldSize
-		if price not in self.sizes :
-			self.sizes[price] = sizeDelta
-		else :
-			self.sizes[price] += sizeDelta
+		if price not in self.orders:
+			self.orders[price]  = sizeDelta
+		else:
+			self.orders[price] += sizeDelta
 
 	def updateMany (self, orders):
 		for order in orders:
 			self.update (order)
 
 	def best (self):
-		return self.sizes.peekitem (index=0 if self.asc else -1)
+		return self.orders.peekitem (index=0 if self.asc else -1)
 
 	def priceFor (self, size):
 		assert size > 0
 		found = 0
-		cost = 0
-		for px in self.sizes if self.asc else reversed (self.sizes):
-			newfind = min (self.sizes[px],size-found)
-			cost += newfind * px
-			found += newfind
+		cost  = 0
+		for p in self.orderIter():
+			newfind  = min (self.orders[p], size-found)
+			cost    += newfind * p
+			found   += newfind
 
 			# min statement should ensure founnd never breaches size
 			assert found <= size
@@ -51,38 +54,36 @@ class HalfOrderBook(object):
 		# order can't be filled on current book
 		return None
 
+	# returns size for price *or better*
+	# for size at price only, use orders[price]
 	def sizeFor (self, price):
 		assert price >= 0
 		found = 0
-		for px in self.sizes if self.asc else reversed (self.sizes):
-			if (px > price and self.asc) \
-				or (px < price and not self.asc):
+		for p in self.orderIter():
+			if (p > price and self.asc) \
+				or (p < price and not self.asc):
 				return found
-			found += self.sizes[px]
+			found += self.orders[p]
 		return found
 
+	def orderIter(self):
+		return self.orders if self.asc else reversed (self.orders)
+
 	def __str__(self):
-		# for the normal (small) size of order books, this should be 
-		# faster than any StringBuilder type optimizations
-		bookStr = ""
-		newLine = ""
-		for px in reversed(self.sizes):
-			bookStr += newLine
-			bookStr += str (self.sizes[px]) + " @ " + str (px)
-			newLine = "\n"
-		return bookStr
+		return "\n".join ([
+			str (self.orders[p]) + " @ " + str (p) for
+			p in reversed(self.orders)
+		])
 
 def test():
-	Asks = HalfOrderBook (asc=True)
-	Bids = HalfOrderBook (asc=False)
-	Asks.updateMany ([ 
+	Asks = HalfOrderBook (asc=True, orders=[ 
 		dict(id=1, price=100, size=6, meta="this"    ),
 		dict(id=2, price=99,  size=3, meta="is"      ),
 		dict(id=3, price=98,  size=2, meta="gdax"    ),
 		dict(id=4, price=97,  size=7, neta="garbage" ),
 	])
 
-	Bids.updateMany ([ 
+	Bids = HalfOrderBook (asc=False, orders=[ 
 		dict(id=5, price=95,  size=1, meta="this"    ),
 		dict(id=6, price=94,  size=5, meta="is"      ),
 		dict(id=7, price=93,  size=5, meta="gdax"    ),
@@ -99,8 +100,11 @@ def test():
 	print ("-------")
 	print (Bids)
 
-	print ("Ask for 10 coins --> " +  str (Asks.priceFor(10)))
-	print ("Spend $99/coin gets me --> " + str (Asks.sizeFor(99)))
-	print ("Bid for 30 coins --> " +  str (Bids.priceFor(30)))
-	print ("At $94/coin, can sell --> " + str (Asks.sizeFor(94)))
-test()
+	print ("Ask for 10 coins --> "       + str (Asks.priceFor (10)))
+	print ("Spend $99/coin gets me --> " + str (Asks.sizeFor  (99)))
+	print ("Bid for 30 coins --> "       + str (Bids.priceFor (30)))
+	print ("At $94/coin, can sell --> "  + str (Bids.sizeFor  (94)))
+
+	print ("All clear!")
+
+#test()
--- orderbook.py
+++ orderbook.py
@@ -1,97 +1,39 @@
-from constants import Unit, OrderType
-import gdax
-
-class Quantity(object):
-	def __init__(self, amount=0, units=Unit.USD):
-		self.amount=amount
-		self.units=units
-
-class Order(object):
-	def __init__(self, size=Quantity(), price=Quantity(), kind=OrderType.BID):
-		self.size=size
-		self.price=price
-		self.kind=kind
-
-		# We do not currently handle negative prices or quantities
-		assert self.size.amount >= 0, "Order size must be greater than or equal to 0"
-		assert self.price.amount >= 0, "Order price must be greater than or equal to 0"
-
-
-	def get_dual(self):
-		"""Describe the order with respect to the inverse kind (bid / offer)"""
-		dualprice = Quantity(amount=1.0/self.price.amount, units=self.size.units)
-		dualsize = Quantity(amount=self.price.amount * self.size.amount, units=self.price.units)
-		dualkind = OrderType.BID if self.kind == OrderType.OFFER else OrderType.OFFER
-		return Order(dualsize, dualprice, dualkind)
-
-	def __str__(self):
-		return "%s for %f %s @ %f %s / %s" % (self.kind.value, self.size.amount, \
-			self.size.units.value, abs(self.price.amount), self.price.units.value, self.size.units.value)
-
-	# TODO 
-	def execute():
-		return "Not Implemented"
+import HalfOrderBook
 
 class OrderBook(object):
-	"""Represents an order book"""
-	def __init__(self, sizeccy=Unit.BTC, priceccy=Unit.USD, bids=[], offers=[]):
-		self.sizeccy=sizeccy
-		self.priceccy=priceccy
-		self.publicclient=gdax.PublicClient()
+	def __init__ (self, priceCcy="USD", sizeCcy="BTC", fee=0.0025):
+		self.orders   = dict(
+			bid=HalfOrderBook(asc=False),
+			ask=HalfOrderBook(asc=True)
+		)
+		self.priceCcy = priceCcy
+		self.sizeCcy  = sizeCcy
+		self.fee	  = fee
 
-		# these will need to be stored in an ordered data structure...
-		# ...do people actually use Min/Max heaps?
-		# self.bids=[]
-		# self.asks=[]
+	# order is a dictionary that contains: side ("bid"/"ask"), id,
+	# price, size
+	def update (self, order):
+		assert "side" in order
+		self.orders[order.side].update (order)
 
-		# Get order book snapshot. <level> paramter described at https://docs.gdax.com/#get-product-order-book
-		raworderbook = self.publicclient.get_product_order_book(self.sizeccy.value + '-' + self.priceccy.value, level=2)
-		print(raworderbook)
+	def updateMany (self, orders):
+		for order in orders:
+			self.update (order)
 
-		# Here we would populate the bids and offers. To start let's use the level2
-		# websocket channel, which is apparently too slow but no dropped messages.
-		# Can update to level3 once we've handled the main cases if necessary
+def test():
+	OB = OrderBook();
 
-	# See https://docs.gdax.com/#real-time-order-book
-	def update(self):
-		return "Not Implemented"
-		# I think the channel should exist outside of the OrderBook, i.e. we will
-		# pass in a message here and handle it (see Book.py in the Pierre Git)
+	OB.updateMany ([ 
+		dict(side="ask", id=1, price=100, size=6, meta="this"    ),
+		dict(side="ask", id=2, price=99,  size=3, meta="is"      ),
+		dict(side="ask", id=3, price=98,  size=2, meta="gdax"    ),
+		dict(side="ask", id=4, price=97,  size=7, neta="garbage" ),
 
-	def __str__(self):
-		return "Not Implemented"
-
-	# For the given order book, what would be the wtd avg price to sell "size"?
-	# Alternatively could make it the total price (i.e. I'm indifferent whether we
-	# multiply by size or not)
-	def bidPrice(self,size=Quantity()):
-		return Quantity()
-
-	# What quantity could I sell at "price"?
-	def bidAmount(self,price=Quantity())
-		# iterate through the bids  (or offers if I'm trying to sell the weaker 
-		# currency, i.e. sell USD on a BTCUSD book) from smallest to largest, summing the 
-		# sizes until you hit price (or 1/price if weaker currency)
-		return Quantity()
-
-	# What is the best bid (agnostic of bid size)
-	def bestBid(self):
-		return Quantity()
-
-	# would then duplicate these methods for Offers
-	# The last methods we would want are ways to actually execute Orders... we'll have
-	# to think about if that will be a property of the book... current framework where
-	# we pass in messages would suggest that the OrderBook should be used to generate
-	# an Order which would then be executed
+		dict(side="bid", id=5, price=95,  size=1, meta="this"    ),
+		dict(side="bid", id=6, price=94,  size=5, meta="is"      ),
+		dict(side="bid", id=7, price=93,  size=5, meta="gdax"    ),
+		dict(side="bid", id=8, price=92,  size=4, neta="garbage" ),
+	])
 
 
-def sanity_check():
-	# testorder = Order(size=Quantity(100, Unit.BTC), price=Quantity(200, Unit.EUR), kind=OrderType.BID)
-	# print(testorder)
-	# print (testorder.get_dual())
-
-	OrderBook()
-
-sanity_check()
-
-
+test()
